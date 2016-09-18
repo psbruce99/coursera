224fc762cf2ae912265c716ccc8bc9a6248ab318
psbruce99 <psbruce99@gmail.com>
2016-09-18 15:45:20 +0200

    new

--- Mod5Sol/js/script.js
+++ Mod5Sol/js/script.js
@@ -97,14 +97,11 @@ function buildAndShowHomeHTML (categories) {
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
     function (homeHtml) {
-
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       // var chosenCategoryShortName = ....
-
-      var = chosenCategoryShortName = chooseRandonCategory(categories).short_name;
-
+      var chosenCategoryShortName =  chooseRandomCategory(categories).short_name;
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -118,18 +115,20 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       // var homeHtmlToInsertIntoMainPage = ....
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName",  "'" +chosenCategoryShortName+"'" );
 
-      chosenCategoryShortName = "'" + chosenCategoryShortName + "'";
-      var homeHtmlToInsertIntoMainPage=insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
-
-
+      // var insertProperty = function (string, propName, propValue) {
+      //   var propToReplace = "{{" + propName + "}}";
+      //   string = string
+      //   .replace(new RegExp(propToReplace, "g"), propValue);
+      //   return string;
+      // };
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
-
-insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
+      insertHtml("#main-content",homeHtmlToInsertIntoMainPage);
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
 }
